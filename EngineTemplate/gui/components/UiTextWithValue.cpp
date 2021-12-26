5eb7f109804cf10e69cbfa2d323309f273607059
Gabriele Padovani <gabriele.padovani@gmail.com>
2021-12-26 10:39:14 +0100

    refactoring

--- EngineTemplate/gui/components/UiTextWithValue.cpp
+++ EngineTemplate/gui/components/UiTextWithValue.cpp
@@ -22,11 +22,13 @@ UiTextWithValue::~UiTextWithValue(){
 }
 
 void UiTextWithValue::Update(int offsetX, int offsetY) {
-    Engine::DeleteTexture(labelTexture); 
-    labelTexture = Engine::LoadTextureFromText(label.c_str() + std::string(" ") + std::to_string(*value));
+    std::string completeLabel = label.c_str() + std::string(" ") + std::to_string(*value);
+    this->w = textLetterSize * (int)completeLabel.length();
+    
+    Engine::DeleteTexture(labelTexture);
+    labelTexture = Engine::LoadTextureFromText(completeLabel);
 }
 
 void UiTextWithValue::Draw(int offsetX, int offsetY){
-    int labelWidth = textLetterSize * (int)(label.length() + std::to_string(*value).length() + 2);
-    Engine::RenderTexture(labelTexture, offsetX, offsetY, labelWidth, h);
+    Engine::RenderTexture(labelTexture, offsetX, offsetY, w, h);
 }
